c388c2447729bceed12cbd0104952438f23687e1
Wesley Jacobs <w.jacobs.632@gmail.com>
2019-12-18 09:26:18 -0800

    added react component working on binary search counter

---
+++ drills.js
@@ -0,0 +1,6 @@
+//Drill 01.1
+// The Search sequence would be 11,5,6, then land on 8
+//Drill 01.2
+// The search sequence would be 11,15,17,18 then fail.
+
+//Drill02
---
+++ searchdrills/src/form.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+
+let dataSet = [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5];
+dataSet.sort((a, b) => (a > b) ? 1 : -1);
+
+export class form extends Component {
+
+    lookForNumberLinear = (num, data) => {
+        for (let i = 0; i < data.length; i++) {
+            if (data[i] === num) {
+                return i + 1;
+            }
+        }
+    }
+
+    lookForNumberBinary = (data, num, start = 0, end = data.length -1, tries) => {
+        if(start > end){
+            console.log('ive reached the end', tries)
+            return -1;
+        }
+        let index = Math.floor((start + end) / 2);
+        let item = data[index];
+
+        if(item === num){
+            return index;
+        } else if( item < num){
+            tries++
+            return this.lookForNumberBinary(data, num, index +1, end, tries)
+        } else if( item > num) {
+            tries++
+            return this.lookForNumberBinary(data, num, start, index -1, tries);
+        }
+        //return tries;
+    }
+
+    handleSubmit = ev => {
+        ev.preventDefault();
+        let input = parseInt(ev.target.input.value);
+
+        alert(this.lookForNumberBinary(dataSet, input));
+    };
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>clicks</label>
+                    <input type='text box' name='input'></input>
+                    <button type='submit'>click</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default form;
